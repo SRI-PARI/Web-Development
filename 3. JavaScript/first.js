05e22cc205b8445324655467d806bf4aab3284da
Sri Pari <paritalasrilakshmi99@gmail.com>
2024-07-06 14:16:43 +0530

    add files to repo

---
+++ 3. JavaScript/first.js
@@ -0,0 +1,62 @@
+console.log("hi");
+/*let fruits=['apple','mango','orange'];
+        fruits.forEach(element => {console.log(element)
+          
+        });
+        for(element of fruits){
+          console.log(element)
+        }
+        for(element in fruits){
+          console.log(element)
+        }""" */
+
+  /*var i=0;
+  while(i<5){
+    console.log(i);
+    i++;
+  }*/
+  /* var i=0;
+   do{
+    console.log(i);
+    i++;
+   }while(i<10);
+   */
+
+   /*var i=0
+   console.log(++i);
+   console.log(i);
+   console.log(i++);
+   console.log(i)*/
+
+/*let a=10
+let b='10'
+console.log(a==b)
+console.log(a===b)
+*/
+
+
+//console.log(a);
+//var a=10
+//console.log(a);
+//let a=10
+
+
+var name='chinna'
+console.log(name+' age is 20');
+console.log(`${name} age is 20`);
+
+var obj={
+  name:'sri',
+  age:10,
+  pari:function(){
+    console.log(`${this.name} paritala`);
+
+  }
+    
+};
+console.log(obj.name)
+obj.pari();
+console.log(obj["age"]);
+console.log(obj.age)
+
+
